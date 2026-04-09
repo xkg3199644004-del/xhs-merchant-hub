5eb6b3dbc54b31f12c2b464325392e8c4dc4a314
xkg3199644004-del <xkg3199644004@gmail.com>
2026-04-09 18:29:36 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import AiChat from './AiChat.vue'
+import type { Theme } from 'vitepress'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('AiChat', AiChat)
+  }
+} satisfies Theme
