be650b3ea456aa2b58633f7d583eafecfbb6efa8
xkg3199644004-del <xkg3199644004@gmail.com>
2026-04-15 14:50:04 +0800

    chore: 清理 theme/index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import AiChat from './AiChat.vue'
-import type { Theme } from 'vitepress'
 
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('AiChat', AiChat)
-  }
-} satisfies Theme
+export default DefaultTheme
